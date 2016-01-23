8fb9a7dc33fa672e40804c7128a14f8fa953a38b
Kamel Arab <kamel.arab@easy-kost.com>
2016-01-23 23:57:04 +0100

    1 Add success

---
+++ www/js/controllers/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by kamel on 23/01/2016.
+ */
+
+angular.module('routerApp.controller')
+    .controller('indexCtrl',['$scope',function($scope){
+        $scope.showSuccess=false;
+    }]);
